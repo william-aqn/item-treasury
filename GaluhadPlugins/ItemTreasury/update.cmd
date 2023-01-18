eef6b77440342def2f5b1c4343b983dc204bf189
william-aqn <dark.crimson.d.c.r.m@gmail.com>
2023-01-18 13:53:57 +0300

    upd

--- GaluhadPlugins/ItemTreasury/update.cmd
+++ GaluhadPlugins/ItemTreasury/update.cmd
@@ -1,3 +1,5 @@
+powershell Set-ExecutionPolicy RemoteSigned -Scope Process
+powershell Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 powershell Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
 powershell -File ".\_update.ps1"
 @REM powershell -File ".\_update_en.ps1"
--- GaluhadPlugins/ItemTreasury/update_en.cmd
+++ GaluhadPlugins/ItemTreasury/update_en.cmd
@@ -1,2 +1,4 @@
+powershell Set-ExecutionPolicy RemoteSigned -Scope Process
+powershell Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 powershell Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
 powershell -File ".\_update_en.ps1"
