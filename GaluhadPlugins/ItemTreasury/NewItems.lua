0a44b93df8d8c5b46ccc0a4f779d306420fb1657
william-aqn <dark.crimson.d.c.r.m@gmail.com>
2024-04-04 08:50:25 +0300

    39.1.1

--- GaluhadPlugins/ItemTreasury/NewItems.lua
+++ GaluhadPlugins/ItemTreasury/NewItems.lua
@@ -1,22 +1,9 @@
-NEWVERSION =  "38.2.0";
+NEWVERSION =  "39.1.1";
 
 _NEWITEMS =
 {
-[1879480524]={[1]="Resilient Cloak of Conflict";[2]="";[3]=45;[4]=3;[5]=3;[6]=false;[7]=false;[8]=0x41246F85;[9]=0x41000004;};
-[1879480525]={[1]="Fateful Cloak of Conflict";[2]="";[3]=45;[4]=3;[5]=3;[6]=false;[7]=false;[8]=0x41246F91;[9]=0x41000004;};
-[1879480526]={[1]="Mending Cloak of Conflict";[2]="";[3]=45;[4]=3;[5]=3;[6]=false;[7]=false;[8]=0x41246F81;[9]=0x41000004;};
-[1879480527]={[1]="Piercing Cloak of Conflict";[2]="";[3]=45;[4]=3;[5]=3;[6]=false;[7]=false;[8]=0x41246FA4;[9]=0x41000004;};
-[1879480528]={[1]="Nimble Cloak of Conflict";[2]="";[3]=45;[4]=3;[5]=3;[6]=false;[7]=false;[8]=0x41246F64;[9]=0x41000004;};
-[1879480529]={[1]="Stunning Cloak of Conflict";[2]="";[3]=45;[4]=3;[5]=3;[6]=false;[7]=false;[8]=0x41246FA4;[9]=0x41000004;};
-[1879480530]={[1]="Durable Cloak of Conflict";[2]="";[3]=45;[4]=3;[5]=3;[6]=false;[7]=false;[8]=0x41246F68;[9]=0x41000004;};
-[1879480535]={[1]="Sea-farer's Primary Essence Box";[2]="Open this box to choose a Sea-farer's Essence of Agility, Might, or Will. The essence you select will be bound to your account.";[3]=27;[4]=2;[5]=0;[6]=false;[7]=false;[8]=0x41154494;[9]=0x41105CB5;};
-[1879480537]={[1]="Cloak of Conflict - Selection Box";[2]="Use to select a cloak. The cloak you select will be bound to your character.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04BB;[9]=0x41105CB5;};
-[1879480540]={[1]="Ill Omens Treasury";[2]="A token of thanks for teaching the Dead to stay that way. NOTE: Contents will vary based on your level.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4124D74C;[9]=0x41105CB5;};
-[1879480625]={[1]="Tome of the Ill Omened Jackal";[2]="Using this item will teach you to summon an ill omened jackal.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
-[1879480626]={[1]="Tome of the Ill Omened Snake";[2]="Using this item will teach you to summon an ill omened snake.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
-[1879480637]={[1]="Tome of the Ill Omened Crab";[2]="Using this item will teach you to summon an ill omened crab.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
-[1879481328]={[1]="Luxurious Crab Cake";[2]="Crab meat breaded and mixed with a variety of local Umbari vegetables and spices. Fried in butter, this dish makes for a tasty and long-lasting snack. The effects of this meal will persist through defeat.";[3]=55;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x4124BCCE;[9]=0x41000E44;};
-[1879481329]={[1]="Luxurious Seasoned Fish Platter";[2]="A freshly steamed fish, cooked with crab meat in a local vegetable stock, this makes for a delicious and filling meal. The effects of this meal will persist through defeat.";[3]=55;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x4124BCCA;[9]=0x41000E44;};
-[1879481330]={[1]="Luxurious Roasted Spiced Vegetable";[2]="Local vegetables from Umbar coated with olive oil and roasted with fish for a rich exotic flavour. The effects of this meal will persist through defeat.";[3]=55;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x4124BCCB;[9]=0x41000E44;};
-[1879481331]={[1]="Luxurious Spiced Goat Shank";[2]="Seasoned goat shanks slowly simmered until tender. The savoury residual juices from the cooking make for an irresistable sauce to accompany the meat. The effects of this meal will persist through defeat.";[3]=55;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x4124BCCC;[9]=0x41000E44;};
+[1879484869]={[1]="Muster in Bej Mâghda";[2]="Allows the Warden to travel to the fishing village in Bej Mâghda.";[3]=105;[4]=2;[5]=0;[6]=false;[7]=true;[8]=0x410031A0;[9]=0x41000003;};
+[1879484870]={[1]="Guide to Bej Mâghda";[2]="Returns you and your Fellowship to the fishing village in Bej Mâghda. This skill is easily interrupted.";[3]=17;[4]=2;[5]=0;[6]=false;[7]=true;[8]=0x410031A0;[9]=0x41000003;};
+[1879484871]={[1]="Sail to Bej Mâghda";[2]="Using this item will teach you to swiftly sail to the fishing village in Bej Mâghda.";[3]=294;[4]=2;[5]=0;[6]=false;[7]=true;[8]=0x410031A0;[9]=0x41000003;};
+[1879484872]={[1]="Return to Bej Mâghda";[2]="You're able to quickly return to the fishing village in Bej Mâghda. This skill is easily interrupted.";[3]=186;[4]=2;[5]=0;[6]=false;[7]=true;[8]=0x410031A0;[9]=0x41000003;};
 };
